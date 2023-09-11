6d5d23d5f2f2188856629c7515d754cc1c73ec4d
sinh <sinh@gmail.com>
2023-09-12 01:46:44 +0700

    Complete Project 1

--- Project1_wiki/encyclopedia/urls.py
+++ Project1_wiki/encyclopedia/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
-
+app_name= 'encyclopedia'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>/", views.greed, name="greed")
+    path("serch/",views.serch, name="serch"),
+    path("create/", views.create, name="create"),
+    path("random/", views.random_page, name="random_page"),
+    path("<str:name>/", views.greed, name="greed"),
+    path("edit/<str:entry>/", views.edit, name="edit")
 ]
--- Project1_wiki/encyclopedia/views.py
+++ Project1_wiki/encyclopedia/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from random import choice
 from . import util
+from django import forms
 
+class CreateTextareaForm(forms.Form):
+    my_title= forms.CharField(
+        widget= forms.TextInput(attrs={
+            'style':'width: 70vw',
+            'placeholder': 'Enter Title'
+        }),
+        label=""
+    )
+    my_textarea= forms.CharField(
+        widget=forms.Textarea(attrs={
+            'style': 'margin-top: 20px; width: 70vw; height: 60vh',
+            'placeholder': 'Enter MarkDown Content'
+        }),
+        label=""
+    )
+
+class EditTextareaForm(forms.Form):
+    textarea_edit= forms.CharField(
+        label="",
+        widget=forms.Textarea(
+            attrs={'style': 'width: 60vw; height: 60vh'}
+        ), 
+        
+    )
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -11,8 +37,82 @@ def index(request):
 
 def greed(request, name):
     for entry in util.list_entries():
-        if name == entry.lower():
+        if name.lower() == entry.lower():
+            html_content= util.convertToHTML(entry)
             return render(request, "encyclopedia/greed.html",{
-                "util": util.get_entry(entry)
+                "util": html_content,
+                "title": name
             })
-    return HttpResponse("The Entry Not Exist!")
+    return render(request, "encyclopedia/greed.html")
+
+def serch(request):
+    serch_result_page= []
+    if request.method=="POST":
+        serch_text= str(request.POST['q'])
+        entries= util.list_entries()
+        
+        for entry in entries:
+            if serch_text.lower()==entry.lower():
+                return HttpResponseRedirect(reverse("encyclopedia:greed", args=[entry]))
+            elif serch_text.lower() in entry.lower():
+                serch_result_page.append(entry)
+        return render(request, "encyclopedia/serch_result_page.html",{
+            "entries": serch_result_page
+        })
+    else:
+        return render(request, "encyclopedia/serch_result_page.html",{
+            "entries": serch_result_page
+        })
+            
+def create(request):
+    check=False
+    if request.method=="POST":
+        form= CreateTextareaForm(request.POST)
+        if form.is_valid():
+            title_data= form.cleaned_data["my_title"]
+            textarea_data= form.cleaned_data["my_textarea"]
+            for entry in util.list_entries():
+                if title_data.lower() == entry.lower():
+                    return render(request, "encyclopedia/create.html",{
+                        "form": form,
+                        "check": True
+                    })
+            util.save_entry(title_data, textarea_data)
+            return HttpResponseRedirect(reverse("encyclopedia:greed", args=[entry]))
+        else:
+            return render(request, "encyclopedia/create.html",{
+                "form": form,
+                "check": False
+            })
+    else:
+        return render(request, "encyclopedia/create.html",{
+                "form": CreateTextareaForm(),
+                "check": False
+            })
+
+def edit(request, entry):
+    if request.method=="POST":
+        form= EditTextareaForm(request.POST)
+        if form.is_valid():
+            edit_content= form.cleaned_data["textarea_edit"]
+            util.save_entry(entry,edit_content)
+            return HttpResponseRedirect(reverse("encyclopedia:greed", args=[entry,]))
+        else:
+            return render(request, "encyclopedia/edit.html",{
+                "form": form,
+                "entry": entry
+            })
+    else:
+        return render(request, "encyclopedia/edit.html",{
+            "form": EditTextareaForm(
+                initial={'textarea_edit': util.get_entry(entry)}
+            ),
+            "entry": entry
+        })
+     
+def random_page(request):
+    entries= util.list_entries()
+    random_entry= choice(entries)
+    return HttpResponseRedirect(reverse("encyclopedia:greed", args=[random_entry]))
+        
+
